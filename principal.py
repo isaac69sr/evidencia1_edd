ed85e7ecdc9352a6741099491029a2376a326325
isaac69sr <isrosales.94@hotmail.com>
2021-10-16 16:48:50 -0500

    se agregan comentarios de las partes del código

--- principal.py
+++ principal.py
@@ -1,5 +1,6 @@
 from os import system
 from operacionesact import operaciones, diccionario
+##importación de librerias, y librerias personalizada
 folio = 0
 datos = ["1","2","3"]
 seleccion = 1
@@ -10,14 +11,13 @@ while seleccion != "3":
     if seleccion == "1":
 
         operaciones.agregar_venta()
-        ##folio = fecha_folio(folio)
-        ##1print(folio)
+        ##CICLO MENU
     if seleccion == "2":
-        if diccionario.__len__() == 0:
+        if diccionario.__len__() == 0: 
             print("sin datos.")
         else:
             print("Ventas Relaizadas(código)\n")
-            operaciones.mostrar_venta()
+            operaciones.mostrar_venta() ##uso de clase
             print("Para ver detallada la información esriba el código de la venta:\n Su selección: ")
             x = input()
             operaciones.detalle_venta(x)
